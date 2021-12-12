3037aba0444cbcdb8d10e9be00e7fbcbe2d5e70f
ArthurO2002 <arthurvoskanyan02@gmail.com>
2021-12-12 19:23:14 +0400

    FEAT: multiple rows sepperate modal file

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,7 @@
 import * as functions from "firebase-functions";
 import {firestore, initializeApp, credential} from "firebase-admin";
 import {WebClient, LogLevel} from "@slack/web-api";
+import inputModal from "./modals/inputModal";
 import axios from "axios";
 const config = functions.config();
 const serviceAcc = config.fire.sdk;
@@ -16,85 +17,7 @@ type IData = Record<string, string>
 const collectionName = "slack-status-update";
 export const myBot = functions.https.onRequest( async (req, res) => {
   if (req.body.command === "/make-status-update") {
-    const modal = {
-      "title": {
-        "type": "plain_text",
-        "text": "My App",
-        "emoji": true,
-      },
-      "submit": {
-        "type": "plain_text",
-        "text": "Submit",
-        "emoji": true,
-      },
-      "type": "modal",
-      "close": {
-        "type": "plain_text",
-        "text": "Cancel",
-        "emoji": true,
-      },
-      "blocks": [
-        {
-          "type": "input",
-          "element": {
-            "initial_value": new Date().toISOString().slice(0, 10),
-            "type": "plain_text_input",
-            "action_id": "date",
-            "placeholder": {
-              "type": "plain_text",
-              "text": "Please insert date",
-            },
-          },
-          "label": {
-            "type": "plain_text",
-            "text": "Date",
-          },
-        },
-        {
-          "type": "input",
-          "element": {
-            "type": "plain_text_input",
-            "action_id": "todayWork",
-            "placeholder": {
-              "type": "plain_text",
-              "text": "Please insert on what you have worked on today",
-            },
-          },
-          "label": {
-            "type": "plain_text",
-            "text": "On what you have worked today",
-          },
-        },
-        {
-          "type": "input",
-          "element": {
-            "type": "plain_text_input",
-            "action_id": "yesterdayWork",
-            "placeholder": {
-              "type": "plain_text",
-              "text": "Please insert on what you have worked on yesterday",
-            },
-          },
-          "label": {
-            "type": "plain_text",
-            "text": "On what you have worked yesterday",
-          },
-        },
-        {
-          "type": "actions",
-          "elements": [
-            {
-              "type": "button",
-              "action_id": "add_option",
-              "text": {
-                "type": "plain_text",
-                "text": "Save",
-              },
-            },
-          ],
-        },
-      ],
-    };
+    const modal = inputModal;
     res.send(modal);
     res.status(200);
   } else {
